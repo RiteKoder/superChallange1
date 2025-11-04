0bcb7bec5c9263e8927e367146a1cdaa5e968fa8
RiteshKrPanda <riteshkrpandawork@gmail.com>
2025-11-04 23:24:00 +0530

    mvp is ready

--- displayStockPrice.js
+++ displayStockPrice.js
@@ -1,8 +1,21 @@
 import getStockPrice from "./fakeStockAPI.js";
 
+const {name,sym} = getStockPrice()
+let price = 0
+let symbolWithPrice = ""
 
- 
+
 const id =  setInterval(() => {
-    console.log(`Name : ${getStockPrice().name}\nSymbol : ${getStockPrice().sym}\nPrice : ${getStockPrice().price}\nTime : ${getStockPrice().price}
+    
+    let nowPrice = getStockPrice().price
+    if (nowPrice > price){
+        symbolWithPrice = nowPrice+" Up"
+    }
+    else {
+        symbolWithPrice = nowPrice+" Down"
+    }
+
+    console.log(`Name : ${name}\nSymbol : ${sym}\nPrice : ${symbolWithPrice}\nTime : ${getStockPrice().time}
     `);
+    price = nowPrice
 }, 1500);
